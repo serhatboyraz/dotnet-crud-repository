cd60c8d12e452c33583630a6b93f5579bca151e8
Serhat Boyraz <serhat.boyraz@detaysoft.com>
2019-05-17 19:14:54 +0300

    dependencies updated

---
+++ CrudRepositoryExample.Data/Context/MasterContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudRepositoryExample.Data.Context
+{
+   public  class MasterContext : DbContext
+    {
+    }
+}
---
+++ CrudRepositoryExample.Data/Model/ProjectRoleModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrudRepositoryExample.Data.Model
+{
+    public class ProjectRoleModel
+    {
+    }
+}
