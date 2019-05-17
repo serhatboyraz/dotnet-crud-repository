0902d32d2d2768106f410e7f6525477921b1de57
Serhat Boyraz <serhat.boyraz@detaysoft.com>
2019-05-17 17:54:57 +0300

    layers generated

---
+++ CrudRepositoryExample.Utils/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrudRepositoryExample.ApiBase
+{
+    public class BaseController<T> : Controller where T : class
+    {
+        [HttpGet("Create")]
+        public IActionResult Create()
+        {
+            return Json(typeof(T).Name);
+        }
+    }
+}
