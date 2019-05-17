14336d6505ba2639a485e07ddc036b251be9574a
Serhat Boyraz <serhat.boyraz@detaysoft.com>
2019-05-17 17:46:36 +0300

    Add project files.

---
+++ CrudRepositoryExample/Controllers/XController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrudRepositoryExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudRepositoryExample.Controllers
+{
+    [Route("X")]
+    public class XController : BaseController<XModel>
+    {
+
+    }
+}
---
+++ CrudRepositoryExample/Controllers/YController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrudRepositoryExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudRepositoryExample.Controllers
+{
+    [Route("Y")]
+    public class YController : BaseController<YModel>
+    {
+
+    }
+}
